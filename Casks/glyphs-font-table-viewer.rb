de4ddd48bb3f23431c2e440eeb14210b554556e3
Aaron Ruan <aaron212cn@outlook.com>
2024-05-09 15:28:17 +0800

    fix versioning

--- Casks/glyphs-font-table-viewer.rb
+++ Casks/glyphs-font-table-viewer.rb
@@ -1,12 +1,13 @@
 cask "glyphs-font-table-viewer" do
-  version :latest
-  sha256 :no_check
+  version "1.3-25"
+  sha256 "74646bd277e88fd454ce81acca46b2c3dd19235be0d41d5a92da48916639024c"
 
   url "https://updates.glyphsapp.com/FontTableViewer/latest.php"
   name "Font Table Viewer"
+  desc "Viewing and comparing OpenType tables of any number of TTFs and CFFs"
   homepage "https://glyphsapp.com/tools/fonttableviewer"
 
-  depends_on macos: ">= :el_capitan"
+  depends_on macos: ">= :sierra"
 
   app "FontTableViewer.app"
 
