810a7bdc8b42dc5c7c1a0f196571278f78e8c2bc
Aaron Ruan <aaron212cn@outlook.com>
2024-05-09 15:30:31 +0800

    revert versioning

--- Casks/glyphs-font-table-viewer.rb
+++ Casks/glyphs-font-table-viewer.rb
@@ -1,6 +1,6 @@
 cask "glyphs-font-table-viewer" do
-  version "1.3-25"
-  sha256 "74646bd277e88fd454ce81acca46b2c3dd19235be0d41d5a92da48916639024c"
+  version :latest
+  sha256 :no_check
 
   url "https://updates.glyphsapp.com/FontTableViewer/latest.php"
   name "Font Table Viewer"
