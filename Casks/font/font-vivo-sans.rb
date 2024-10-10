2032876899552bde45ac129b8d63947d859d6466
Aaron Ruan <aaron212cn@outlook.com>
2024-10-10 12:07:47 +0800

    font-vivo-sans

---
+++ Casks/font/font-vivo-sans.rb
@@ -0,0 +1,18 @@
+cask "font-vivo-sans" do
+  version :latest
+  sha256 :no_check
+  
+  url "https://h5-pre.vivo.com.cn/pcvivo/OS5-font/vivo%20Sans.zip"
+  name "Vivo Sans"
+  homepage "https://www.vivo.com/originos"
+
+  font "vivo Sans/vivo Sans/vivoSans-ExtraBold.ttf"
+  font "vivo Sans/vivo Sans/vivoSans-ExtraLight.ttf"
+  font "vivo Sans/vivo Sans/vivoSans-Heavy.ttf"
+  font "vivo Sans/vivo Sans/vivoSans-Light.ttf"
+  font "vivo Sans/vivo Sans/vivoSans-Medium.ttf"
+  font "vivo Sans/vivo Sans/vivoSans-Regular.ttf"
+  font "vivo Sans/vivo Sans/vivoSans-Thin.ttf"
+  font "vivo Sans/vivo Sans/vivoSansSCVF-500.ttf"
+  font "vivo Sans/vivo Sans/vivoSansSCVF-600.ttf"
+end
