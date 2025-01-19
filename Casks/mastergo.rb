6459df0ca890b0ed6419321e1a7d5f608182364f
Aaron Ruan <i@ar212.com>
2025-01-19 17:59:09 +0800

    mastergo 1.9.1

--- Casks/mastergo.rb
+++ Casks/mastergo.rb
@@ -1,9 +1,9 @@
 cask "mastergo" do
   arch arm: "macos-arm", intel: "macos"
 
-  version "1.8.2"
-  sha256 intel: "3fe5f5824f07bdb4480bb0bf06991f5c0db5098f00e5a9bf14f9194013a82439",
-         arm:   "42e970f0f5a303018cfd7fe7bd4b7aac2cead341464db02e295a48c1307d943b"
+  version "1.9.1"
+  sha256 intel: "dde3a4c0be3aef13e72e271198253ffc140fee7d082109bb8561e05ac732c4fd",
+         arm:   "605851c0760bbf198065b7bfc6cee22095ecd05928b6a8ba284e8a3e7f5492c1"
 
   url "https://static.mastergo.com/plugins/desktop/#{arch}/MasterGo-#{version}.dmg"
   name "MasterGo"
