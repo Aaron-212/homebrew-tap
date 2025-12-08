97c8b190882f00c80a055835c3fd5bcd13edc176
Aaron Ruan <i@ar212.com>
2025-12-08 19:57:39 +0800

    bump longbridge to 0.10.0

--- Casks/longbridge.rb
+++ Casks/longbridge.rb
@@ -1,9 +1,9 @@
 cask "longbridge" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.2.1"
-  sha256 arm:   "fcf91213bf62767be0beda51bf9601a10d51f1fdaacefbb1c509f55d75402213",
-         intel: "bbd2a2565b740094bdc754c99e039372143efebe1bae8e7c0877248aa604bf35"
+  version "0.10.0"
+  sha256 arm:   "ef5c558f4f3671280e0070da3e9589f1ef99b0beccdcce54e9943d2dc0d69532",
+         intel: "f38a6ca5b1698ff5cf5c3977e1f819b3279dfeedf6260649186b8c814cc1ec68"
 
   url "https://assets.lbctrl.com/github/release/longbridge-desktop/stable/longbridge-v#{version}-macos-#{arch}.dmg",
       verified: "assets.lbctrl.com/github/release/longbridge-desktop/"
@@ -16,7 +16,7 @@ cask "longbridge" do
     regex(/href=.*?longbridge[._-]v?(\d+(?:\.\d+)+)-macos-#{arch}\.dmg/i)
   end
 
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :big_sur"
 
   app "Longbridge.app"
 
