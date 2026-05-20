b44cd2f275cfee75c59552d32ef8d7b036aed528
Aaron Ruan <i@ar212.com>
2026-05-20 23:12:07 +0800

    airi 0.10.2

---
+++ Casks/airi.rb
@@ -0,0 +1,37 @@
+cask "airi" do
+  arch arm: "arm64", intel: "x64"
+
+  version "0.10.2"
+  sha256 arm:   "494c2c441e5993fa9d87673f6de6c493d7bcd9a952af7cdd621cd499349ad341",
+         intel: "b0e5b3bf3a3623ab0b2df335fe43e258cf97e287741898952e6a28c4f2a23713"
+
+  url "https://github.com/moeru-ai/airi/releases/download/v#{version}/AIRI-#{version}-darwin-#{arch}.dmg"
+  name "Airi"
+  desc "Self-hosted AI companion"
+  homepage "https://github.com/moeru-ai/airi/"
+
+  livecheck do
+    url "https://github.com/moeru-ai/airi/releases/latest"
+    strategy :github_latest
+  end
+
+  depends_on macos: :monterey
+
+  app "Airi.app"
+
+  zap trash: ""
+
+  caveats do
+    app_name = @cask.name.first
+
+    <<~EOS
+      #{app_name} is not signed by an Apple Developer certificate.
+      This means the app is probably signed with an Ad-Hoc profile.
+      Gatekeeper may block this app from opening.
+
+      If you know what you are doing, run:
+        sudo xattr -c '#{appdir}/#{app_name}.app'
+      in the terminal.
+    EOS
+  end
+end
